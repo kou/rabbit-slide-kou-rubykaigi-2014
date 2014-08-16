4a1fbc7da683d8ad898663771900c7ed14d4d982
Kouhei Sutou <kou@clear-code.com>
2014-08-16 19:26:14 +0900

    benchmark concurrency: remove needless --host option
    It should be bound to 0.0.0.0.

--- benchmark/concurrency/server-thread.rb
+++ benchmark/concurrency/server-thread.rb
@@ -5,20 +5,15 @@ require "ostruct"
 require "socket"
 
 options = OpenStruct.new
-options.host = "127.0.0.1"
 options.port = 2929
 parser = OptionParser.new
-parser.on("--host=HOST",
-          "(#{options.host})") do |host|
-  options.host = host
-end
 parser.on("--port=PORT", Integer,
           "(#{options.port})") do |port|
   options.port = port
 end
 parser.parse!
 
-server = TCPServer.new(options.host, options.port)
+server = TCPServer.new(options.port)
 loop do
   Thread.new(server.accept) do |client|
     data = client.read(1)
