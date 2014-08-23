2508d243bf34529434d8f5c4f0c8b39fefb04c79
Kouhei Sutou <kou@clear-code.com>
2014-08-23 13:23:59 +0900

    Support parsing JSON

--- benchmark/echo/server-coolio-worker.rb
+++ benchmark/echo/server-coolio-worker.rb
@@ -3,10 +3,14 @@
 require "optparse"
 require "ostruct"
 require "coolio"
+require "oj"
+require "yajl"
+require "json"
 
 options = OpenStruct.new
 options.port = 22929
 options.concurrency = 8
+options.json_parser = nil
 parser = OptionParser.new
 parser.on("--port=PORT", Integer,
           "(#{options.port})") do |port|
@@ -15,6 +19,12 @@ end
 parser.on("--concurrency=N", Integer,
           "(#{options.concurrency})") do |concurrency|
   options.concurrency = concurrency
+end
+json_parsers = [:json, :yajl, :oj]
+parser.on("--json-parser=PARSER", json_parsers,
+          "available parsers: #{json_parsers.inspect}",
+          "(#{options.json_parser})") do |json_parser|
+  options.json_parser = json_parser
 end
 parser.parse!
 
@@ -29,6 +39,14 @@ options.concurrency.times do
 
     server = Coolio::TCPServer.new(raw_server) do |client|
       client.on_read do |data|
+        case options.json_parser
+        when :json
+          JSON.parse(data)
+        when :yajl
+          Yajl::Parser.parse(data)
+        when :oj
+          Oj.load(data)
+        end
         write(data)
       end
     end
--- benchmark/echo/server-coolio.rb
+++ benchmark/echo/server-coolio.rb
@@ -3,19 +3,37 @@
 require "optparse"
 require "ostruct"
 require "coolio"
+require "oj"
+require "yajl"
+require "json"
 
 options = OpenStruct.new
 options.port = 22929
+options.json_parser = nil
 parser = OptionParser.new
 parser.on("--port=PORT", Integer,
           "(#{options.port})") do |port|
   options.port = port
+end
+json_parsers = [:json, :yajl, :oj]
+parser.on("--json-parser=PARSER", json_parsers,
+          "available parsers: #{json_parsers.inspect}",
+          "(#{options.json_parser})") do |json_parser|
+  options.json_parser = json_parser
 end
 parser.parse!
 
 loop = Coolio::Loop.default
 server = Coolio::TCPServer.new(nil, options.port) do |client|
   client.on_read do |data|
+    case options.json_parser
+    when :json
+      JSON.parse(data)
+    when :yajl
+      Yajl::Parser.parse(data)
+    when :oj
+      Oj.load(data)
+    end
     write(data)
   end
 end
